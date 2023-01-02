45d68df2774cbec221dbbe4333adcee9ac16d9c0
Mayuri Kothari <115354882+MAYURIKOTHARI299@users.noreply.github.com>
2023-01-02 17:06:33 +0530

    Todo List App

---
+++ Projects/ToDo List App/script.js
@@ -0,0 +1,35 @@
+
+function addTask () {
+    var input = document.getElementById("input");
+    // get current text from input field
+    var newTask = input.value;
+    // only add new item to list if some text was entered 
+    if (newTask != "") {
+        // create new HTML list item
+        var item = document.createElement("li");
+        // add HTML for buttons and new task text
+        // Note, need to use '' because of "" in HTML
+        item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)"  value="&#x2713;" /> ' + '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' + newTask;
+
+        // add new item as part of existing list
+        document.getElementById("tasks").appendChild(item); 
+        input.value="";
+        input.placeholder="Enter task...";
+
+        /* Step 4 below here */ 
+    }
+}
+
+// change styling used for given item
+function markDone (item) {
+    item.className = 'finished';
+}
+
+/* Step 7 below here */
+function remove (item) {
+    // remove item completely from document
+    item.remove();
+    item.className= finished;
+}
+
+/* Step 11 below here */
