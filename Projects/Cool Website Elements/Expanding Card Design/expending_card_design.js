c0bc50abc5c22d09fe3748afc1468237c016d7ee
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-01-02 22:51:31 +0530

    Cool Website Elements
    Cool Website Elements

---
+++ Projects/Cool Website Elements/Expanding Card Design/expending_card_design.js
@@ -0,0 +1,14 @@
+const cards =document.querySelectorAll('.card')
+
+cards.forEach((card) =>{
+    card.addEventListener('click', () =>{
+        removeActive()
+        card.classList.add('active')
+    })
+})
+
+function removeActive () {
+    cards.forEach(card =>{
+        card.classList.remove('active')
+    })
+}
