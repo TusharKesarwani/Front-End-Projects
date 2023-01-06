530f0a854f40e6605280f58f506a46568da65cb3
Mohamed Mafaz P R <113255500+MohamedMafaz@users.noreply.github.com>
2023-01-06 21:16:42 +0530

    Add files via upload

---
+++ Projects/Sushi Restaurant Website/assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
