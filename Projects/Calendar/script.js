1ea17c9e1611ba1b48228e653c567bc11b588db4
Khushi Garg <khushigarg.64901@gmail.com>
2023-01-15 20:58:07 +0530

    final

--- Projects/Calendar/script.js
+++ Projects/Calendar/script.js
@@ -2,12 +2,10 @@ const daysTag = document.querySelector(".days"),
     currentDate = document.querySelector(".current-date"),
     prevNextIcon = document.querySelectorAll(".icons span");
 
-// getting new date, current year and month
 let date = new Date(),
     currYear = date.getFullYear(),
     currMonth = date.getMonth();
 
-// storing full name of all months in array
 const months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
