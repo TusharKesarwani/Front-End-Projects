e0d9194afb5561a275317600caf21e5a4b56677a
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-01-21 15:11:30 +0530

    Added Simon game

---
+++ Projects/Simon Game/game.js
@@ -0,0 +1,82 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if(userClickedPattern.length === gamePattern.length) {
+
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+
+        $("#level-title").html("Game Over,<span style='background-color: white;'>Press Any Key to Start the Game</span>");
+
+        $("#level-title").css("fontSize", "3.3rem")
+
+        startOver();
+    }
+}
+
+function nextSequence() {
+
+    
+    $("body").removeClass("game-over");
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100); 
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
