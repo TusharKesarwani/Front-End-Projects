8128910a8d89df17fa06bcfc6cfa5f5a879d6ac6
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2023-01-27 18:37:42 +0530

    added projects search functionality

---
+++ projects.js
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        "title":"Product Launching Website",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/product%20launching%20website.png",
+        "description":"This is a simple product launching website made using HTML, CSS and JavaScript. It is a simple landing page used to showcase the newly launched prodcts.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Product%20Launching%20Website",
+        "project-link":"Projects/Product Launching Website/index.html"
+    },
+    {
+        "title":"Calculator",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/calculator.png",
+        "description":"This is a simple calculator made using HTML, CSS and JavaScript. It is a basic calculator which can perform basic operations like addition, subtraction, multiplication and division.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Calculator",
+        "project-link":"Projects/Calculator/index.html"
+    },
+    {
+        "title":"Tindog website",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/tindog_website1.png",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/TinDog%20Website",
+        "project-link":"Projects/TinDog Website/index.html"
+    },
+    {
+        "title":"The Hangman Game",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/The-Hangman-Game.jpg",
+        "description":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam ad suscipit minima possimus nobis. Id, laborum reiciendis. Voluptatum voluptatibus, nostrum nisi atque dolorum fugiat explicabo similique, placeat dolores voluptas officiis!        ",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/The-Hangman-Game",
+        "project-link":"Projects/The-Hangman-Game/index.html"
+    },
+    {
+        "title":"Coming Soon",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/coming_soon.png",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
+        "github-link":"#",
+        "project-link":"#"
+    },
+    {
+        "title":"Coming Soon",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/coming_soon.png",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
+        "github-link":"#",
+        "project-link":"#"
+    }
+]
+
+export default projects;
