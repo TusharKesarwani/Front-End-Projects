edd80e234bb95e80d62038ff950d98a069d70a52
Khushi Garg <khushigarg.64901@gmail.com>
2023-01-30 22:49:33 +0530

    maze game

--- Projects/Gooey Cursor/javascript.js
+++ Projects/Gooey Cursor/javascript.js
@@ -1,14 +1,14 @@
 console.clear();
 
-const curlen = 20;
+const TAIL_LENGTH = 20;
 
 const cursor = document.getElementById('cursor');
 
 let mouseX = 0;
 let mouseY = 0;
 
-let circles;
-let history = Array(curlen).fill({x: 0, y: 0});
+let cursorCircles;
+let cursorHistory = Array(TAIL_LENGTH).fill({x: 0, y: 0});
 
 function onMouseMove(event) {
   mouseX = event.clientX;
@@ -16,28 +16,28 @@ function onMouseMove(event) {
 }
 
 function initCursor() {
-  for (let i = 0; i < curlen; i++) {
+  for (let i = 0; i < TAIL_LENGTH; i++) {
     let div = document.createElement('div');
     div.classList.add('cursor-circle') ;
     cursor.append(div);
   }
-  circles = Array.from(document.querySelectorAll('.cursor-circle'));
+  cursorCircles = Array.from(document.querySelectorAll('.cursor-circle'));
 }
 
 function updateCursor() {  
-  history.shift();
-  history.push({ x: mouseX, y: mouseY });
+  cursorHistory.shift();
+  cursorHistory.push({ x: mouseX, y: mouseY });
     
-  for (let i = 0; i < curlen; i++) {
-    let current = history[i];
-    let next = history[i + 1] || history[curlen - 1];
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    let current = cursorHistory[i];
+    let next = cursorHistory[i + 1] || cursorHistory[TAIL_LENGTH - 1];
     
-    let diffx = next.x - current.x;
-    let diffy = next.y - current.y;
+    let xDiff = next.x - current.x;
+    let yDiff = next.y - current.y;
     
-    current.x += diffx * 0.35;
-    current.y += diffy * 0.35;
-    circles[i].style.transform = `translate(${current.x}px, ${current.y}px) scale(${i/curlen})`;  
+    current.x += xDiff * 0.35;
+    current.y += yDiff * 0.35;
+    cursorCircles[i].style.transform = `translate(${current.x}px, ${current.y}px) scale(${i/TAIL_LENGTH})`;  
   }
   requestAnimationFrame(updateCursor)
 }
