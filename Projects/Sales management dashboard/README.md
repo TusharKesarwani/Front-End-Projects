85195baf953a9ad2786c81c423efca52557317a1
Alisha <722alisha@gmail.com>
2023-01-31 19:29:27 +0530

    final commit

---
+++ Projects/Sales management dashboard/README.md
@@ -0,0 +1,15 @@
+# NAME AND INTRODUCTION OF THE PROJECT
+# Sales management Dashboard
+I have designed a simple yet attractive UI for dashboard template of sales management . I have choosen a minimal theme for it and it is responsive .
+
+# TECH STACK :
+HTML , CSS , JAVASCRIPT and charts are built using ApexCharts 3.
+
+# SNAPSHOT OF THE WEBSITE:
+
+
+# CONTRIBUTED BY:
+ALISHA BHARTI
+
+Hope , it would be useful !!!
+
---
+++ Projects/Sales management dashboard/js/scripts.js
@@ -0,0 +1,252 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [10, 8, 6, 4, 2],
+    name: "Products",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// AREA CHART
+var areaChartOptions = {
+  series: [{
+    name: "Purchase Orders",
+    data: [31, 40, 28, 51, 42, 109, 100],
+  }, {
+    name: "Sales Orders",
+    data: [11, 32, 45, 32, 34, 52, 41],
+  }],
+  chart: {
+    type: "area",
+    background: "transparent",
+    height: 350,
+    stacked: false,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#00ab57", "#d50000"],
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    gradient: {
+      opacityFrom: 0.4,
+      opacityTo: 0.1,
+      shadeIntensity: 1,
+      stops: [0, 100],
+      type: "vertical",
+    },
+    type: "gradient",
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  markers: {
+    size: 6,
+    strokeColors: "#1b2635",
+    strokeWidth: 3,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      offsetY: 5,
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: 
+  [
+    {
+      title: {
+        text: "Purchase Orders",
+        style: {
+          color: "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: "Sales Orders",
+        style: {
+          color:  "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
