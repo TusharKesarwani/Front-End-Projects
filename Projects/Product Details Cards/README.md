201a5d355f125121008838bf8d707e177a6b65d1
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2023-01-31 15:22:59 +0530

    Made the product details cards by using html css javascript

---
+++ Projects/Product Details Cards/README.md
@@ -0,0 +1,5 @@
+<!-- This project is a part of open source contribution Program SWOC 3.0 -->
+
+I make a products detail card by using HTML CSS and JAVASCRIPT. 
+
+-- Vimal Anand
---
+++ Projects/Product Details Cards/script.js
@@ -0,0 +1,21 @@
+
+const allHoverImages = document.querySelectorAll('.hover-container div img');
+const imgContainer = document.querySelector('.img-container');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('mouseover', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img) => {
+        img.parentElement.classList.remove('active');
+    });
+}
