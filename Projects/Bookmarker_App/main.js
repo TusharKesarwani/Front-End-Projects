8d0762778820afa2ef289ff834d5d34411aedc7a
Muskan Ali Qureshi <qmuskan467@gmail.com>
2023-02-05 13:10:49 +0530

    Update main.js

--- Projects/Bookmarker_App/main.js
+++ Projects/Bookmarker_App/main.js
@@ -9,75 +9,40 @@ function savebookmark(e) {
         return false;
     }
 
-    /*if(!sitename || !siteurl) {
-        alert('Please fill the form !!!');
-        return false;
-    }
-
-    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expression);
-
-    if(!siteurl.match(regex)){
-        alert('Please use a valid URL')
-        return false;
-    }*/
-
-    //console.log(sitename);
-    //console.log(siteurl);
     // creating object to put in local storage
     var bookmark = {
         name: sitename,
         url: siteurl
     }
 
-    //console.log(bookmark);
-    /*
-    //local-storage test
-    localStorage.setItem('test', 'hello guys');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-    */
-
     if (localStorage.getItem('bookmarks') === null) {
         var bookmarks = [];
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    } else {
+    }
+    else {
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
         bookmarks.push(bookmark);
-
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
     document.getElementById('myform').reset();
-
     fetchbookmarks();
     //prevents form submission
     e.preventDefault();
 }
 
-function deletebookmark(url) {
-    //console.log(url);
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-//     for (var i = 0; i < bookmarks.length; i++) {
-//         if (bookmarks[i].url == url) {
-//             bookmarks.splice(i, 1);
-//         }
-//     }
+function deletebookmark(i) {
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
     bookmarks.splice(i, 1);
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     fetchbookmarks();
 }
 
 //fetch bookmarks
-
 function fetchbookmarks() {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
     //console.log(bookmarks);
-
     var bookmarkresults = document.getElementById('bookmarksresults');
 
     bookmarkresults.innerHTML = '';
@@ -85,8 +50,8 @@ function fetchbookmarks() {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
 
-        bookmarkresults.innerHTML += `<div class="well"> +
-            <div class="row">
+        bookmarkresults.innerHTML += `<div class="well"> 
+        <div class="row">
             <div class="col-9">
                 <h5>${bookmarks[i].name}</h5>    
             </div>
@@ -99,12 +64,15 @@ function fetchbookmarks() {
         </div>`;
     }
 }
+
+// Validate Form
 function validateform(sitename, siteurl) {
     if (!sitename || !siteurl) {
         alert('Please fill the form !!!');
         return false;
     }
 
+    // using regular expression here  for valid url 
     var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(expression);
 
@@ -112,6 +80,5 @@ function validateform(sitename, siteurl) {
         alert('Please use a valid URL')
         return false;
     }
-
     return true;
 }
