e37314cdd63e3824c210082f9b24ad045b867d38
DinkyRajpoot56 <111672121+DinkyRajpoot56@users.noreply.github.com>
2023-02-11 18:33:57 +0530

    Add files via upload

---
+++ BakeryApp.js
@@ -0,0 +1,51 @@
+// MENU TOGGLE
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('show');
+});
+
+// CLOSE THE NAV WHEN NAVLNKS ARE CLICKED
+let navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(function (navLink) {
+    navLink.addEventListener('click', function () {
+        body.classList.remove('show');
+    })
+})
+
+// SCROLL REVEAL
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
