56ec6481bed2b6089187104f8facb46f62d5db99
Nikhil Srivastava <108658668+nikhilsrv@users.noreply.github.com>
2023-02-18 15:56:04 +0530

    Update insertion.js

--- Projects/Sorting Visualizer/insertion.js
+++ Projects/Sorting Visualizer/insertion.js
@@ -6,13 +6,13 @@ async function Insertionsort(heightarray,al)
         let current=heightarray[i]
         let j=i-1;
         while(heightarray[j]>current&&j>=0)
-        {   await waitforme(v)//wait for 2 miliseconds
+        {   await waitforme(v)//wait for  miliseconds
             heightarray[j+1]=heightarray[j]
             document.getElementById(array_of_bars[j+1].id).style.height=`${heightarray[j]}px`
             document.getElementById(array_of_bars[j+1].id).style.borderLeft="2px solid white"
             j--
         }
-        await waitforme(v)//wait for 2 miliseconds
+        await waitforme(v)//wait for  miliseconds
         heightarray[j+1]=current
         document.getElementById(array_of_bars[j+1].id).style.height=`${current}px`
         document.getElementById(array_of_bars[j+1].id).style.borderLeft="2px solid white"
