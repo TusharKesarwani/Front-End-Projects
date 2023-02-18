e64f894850c493cf314bca40c84f5d5e0ab938b0
Nikhil Srivastava <108658668+nikhilsrv@users.noreply.github.com>
2023-02-18 15:55:09 +0530

    Update count.js

--- Projects/Sorting Visualizer/count.js
+++ Projects/Sorting Visualizer/count.js
@@ -22,7 +22,7 @@ async function countsort(heightarray,al)
     for(let i=0;i<al;i++)
     {
     heightarray[i]=output[i]
-    await waitforme(v);//wait for 2 miliseconds
+    await waitforme(v);//wait for  miliseconds
     document.getElementById(array_of_bars[i].id).style.height=`${heightarray[i]}px`
     document.getElementById(array_of_bars[i].id).style.borderLeft="2px solid red"
     }
