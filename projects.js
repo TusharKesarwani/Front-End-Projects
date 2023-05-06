df969f31b0e521bb772d7edfc23202341f985508
HeySkidee <harshitojha46@gmail.com>
2023-05-07 02:07:02 +0530

    list projects, fix links

--- projects.js
+++ projects.js
@@ -1,11 +1,146 @@
 const projects = [
     {
-        "title":"Product Launching Website",
+        "title":"Coding Community Website",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"img/projects/product%20launching%20website.png",
-        "description":"This is a simple product launching website made using HTML, CSS and JavaScript. It is a simple landing page used to showcase the newly launched prodcts.",
-        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Product%20Launching%20Website",
-        "project-link":"Projects/Product Launching Website/index.html"
+        "img":"img/projects/coding community.png",
+        "description":"Competitive Programming community website for Codechef MMMUT Chapter which is a community of coding enthusiasts that aims to enhance coding skills of its members required for their successful career.",
+        "github-link":"https://github.com/TusharKesarwani/CodeChefMMMUTChapter",
+        "project-link":"https://codechef-mmmut-chapter.netlify.app"
+    },
+    {
+        "title":"Crypto Trading Landing Page",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/crypto dashboard.png",
+        "description":"Experience a sleek and user-friendly dashboard for your crypto trading. Stay updated with real-time market data and make informed decisions for your investments.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Crypto%20Dashboard",
+        "project-link":"Projects/Crypto%20Dashboard/index.html"
+    },
+    {
+        "title":"Hoolix - Dogital Agency Website",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/hoolix.png",
+        "description":"I have designed a template for a digital agency website in which there is features like Home About Services Features Blog Contact Us",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/hoolix-digital%20agency%20website",
+        "project-link":"Projects/hoolix-digital%20agency%20website/index.html"
+    },
+
+
+
+    {
+        "title":"Drawing App",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/sketch.png",
+        "description":"This a simple Drawing Application where users can create funny drawings using different shapes and different colors.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/DRAWING%20APP",
+        "project-link":"Projects/DRAWING%20APP/sketch.html"
+    },    
+    {
+        "title":"Image Editor",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/image editor.png",
+        "description":"Its a basic Image Editor in JavaScript,users can apply different filters to their image like grayscale, inversion, saturation, and adjust the image brightness. Additionally, users can also rotate or flip the images and save their edited images.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/image%20editor",
+        "project-link":"Projects/image%20editor/index.html"
+    },
+    {
+        "title":"QR Code Generator",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/qr code.png",
+        "description":"Javascript is used mainly manipulate the entered data for generating QR code. Here API is used to create the QR code.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/QR%20Code%20Generator",
+        "project-link":"Projects/QR%20Code%20Generator/Index.html"
+    },
+
+
+
+    {
+        "title":"Zomato Website Clone",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/zomato.png",
+        "description":"A Zomato Landing Page Made using HTML and CSS.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Zomato-Landing-Page",
+        "project-link":"Projects/Zomato-Landing-Page/index.html"
+    },
+    {
+        "title":"Myntra Website Clone",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/myntra.png",
+        "description":"Myntra Clone website created using HTML, CSS is a myntra-like website where users can seamlessly search for clothes for kids, mens and womens etc and also get wonderful products available online on the website.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/myntra-clone",
+        "project-link":"Projects/myntra-clone/index.html"
+    },
+    {
+        "title":"E-Commerce Website",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/ecommerce.png",
+        "description":"In this Project I have created the landing page of a e-commerce website. In this website any user can buy cloths, shoes as well household things. Tech stacks used are HTML CSS and Bootstrap.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/E-commerce%20Landing%20Page",
+        "project-link":"Projects/E-commerce%20Landing%20Page/index.html"
+    },
+
+
+
+    {
+        "title":"Analytics Dashboard",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/dashboard.png",
+        "description":"I have designed a simple yet attractive UI Template of Management Dashboard. I have choosen a minimal theme for it and it is responsive. It can be used as a template for making dashboard in any website .",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/dashboard",
+        "project-link":"Projects/dashboard/index.html"
+    },
+    {
+        "title":"3D Maze",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/find-the-key.png",
+        "description":"This website is a 3D game created with HTML, CSS, and JS. Using arrow keys to move and the spacebar to jump, players navigate through a maze to find a key.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/3D%20Maze",
+        "project-link":"Projects/3D%20Maze/index.html"
+    },
+    {
+        "title":"Windows 11 Clone",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/windows 11.png",
+        "description":"In this Project, I have made a UI of Latest Windows11 Desktop and it has many features like date and time , task bar menu ( widegts , search bar )etc.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/windows%2011",
+        "project-link":"Projects/windows%2011/index.html"
+    },
+
+
+
+    {
+        "title":"Modern Credit Card Form",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/card.png",
+        "description":"Modern credit Card form Webpage design by using Html,Css & java Script.it is very Intractive and fully responsive.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Modern%20credit%20Card%20form%20Webpage",
+        "project-link":"Projects/Modern%20credit%20Card%20form%20Webpage/index.html"
+    },
+    {
+        "title":"3D Slider component",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/slide.png",
+        "description":"The 'voyage-slider-gsap' is a lightweight and customizable slider component that allows you to create responsive and touch-enabled sliders for your website or web application. It is built using HTML, CSS, and JavaScript",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Voyage%20Slider%20Gsap",
+        "project-link":"Projects/Voyage%20Slider%20Gsap/index.html"
+    },
+    {
+        "title":"Login Page Template",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/login page.png",
+        "description":"Stylish login page made with HTML, CSS and JavaScript. Enhance your website's user experience with this sleek design.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/SignIn%20Page",
+        "project-link":"Projects/SignIn%20Page/index.html"
+    },
+
+
+    
+    {
+        "title":"Atmospheric Air Quality Checker",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/air-quality.png",
+        "description":"Check the air quality of your location with Atmospheric Air Quality Checker. Simply enter your location and get the Air Quality Index along with the concentration of pollutants in the air.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Air-Quality",
+        "project-link":"Projects/Air-Quality/index.html"
     },
     {
         "title":"Calculator",
@@ -16,37 +151,54 @@ const projects = [
         "project-link":"Projects/Calculator/index.html"
     },
     {
-        "title":"Tindog website",
+        "title":"Analog clock",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"img/projects/tindog_website1.png",
-        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
-        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/TinDog%20Website",
-        "project-link":"Projects/TinDog Website/index.html"
+        "img":"img/projects/clock.png",
+        "description":"Check out this simple and elegant analog clock made with HTML, CSS, and JavaScript. See the time in a classic and traditional way.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Analog%20Clock",
+        "project-link":"Projects/Analog%20Clock/index.html"
+    },
+
+
+
+    {
+        "title":"Product Launching Website",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/product launching website.png",
+        "description":"This is a simple product launching website made using HTML, CSS and JavaScript. It is a simple landing page used to showcase the newly launched prodcts.",
+        "github-link":"https://github.com/TusharKesarwani/Product_Launching_Website",
+        "project-link":"https://tusharkesarwani.github.io/Product_Launching_Website/"
     },
     {
-        "title":"The Hangman Game",
+        "title":"Animated Card",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"img/projects/The-Hangman-Game.jpg",
-        "description":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam ad suscipit minima possimus nobis. Id, laborum reiciendis. Voluptatum voluptatibus, nostrum nisi atque dolorum fugiat explicabo similique, placeat dolores voluptas officiis!        ",
-        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/The-Hangman-Game",
-        "project-link":"Projects/The-Hangman-Game/index.html"
+        "img":"img/projects/animation card.png",
+        "description":"An animated portfolio card with sleek design made with HTML and CSS.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/Animated%20Card",
+        "project-link":"Projects/Animated%20Card/animated_card.html"
     },
     {
-        "title":"Coming Soon",
+        "title":"Music Player",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"img/coming_soon.png",
-        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
-        "github-link":"#",
-        "project-link":"#"
+        "img":"img/projects/Music Player.png",
+        "description":"My music player allows you to listen music/song of your own choice. I have used javascript to control the music time, play next or previous song, pause & play. For now, I have only added 5 music but it can be enhanced by adding your favourite playlist.",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects/MusicPlayer%20using%20JavaScript",
+        "project-link":"Projects/MusicPlayer%20using%20JavaScript/index.html"
     },
+
+
+
     {
-        "title":"Coming Soon",
+        "title":"Explore Projects on GitHub Repo",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"img/coming_soon.png",
-        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat provident incidunt fuga, nihil ex aperiam, debitis minus a recusandae quia, blanditiis ad eius modi! Minima sunt dolor ut minus totam.",
-        "github-link":"#",
-        "project-link":"#"
-    }
+        "img":"img/projects/explore.png",
+        "description":"Explore more cool projects in the Porjects list on our github repo :)",
+        "github-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects",
+        "project-link":"https://github.com/TusharKesarwani/Front-End-Projects/tree/main/Projects"
+    },
+
+
+    
 ]
 
 export default projects;
