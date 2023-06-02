be48e8235f8c7ca8b95dfad389e3fe4c53fd324a
2020B1531074 <varunbaman734@gmail.com>
2023-06-02 23:06:20 +0530

    Remove extra code and improve syntax

--- Projects/BMICalculator/scripts.js
+++ Projects/BMICalculator/scripts.js
@@ -1,32 +1,29 @@
-document.getElementById('sub').addEventListener('click',function(){
+document.getElementById('sub').addEventListener('click', function () {
     let h = document.getElementById('height').value;
     let w = document.getElementById('weight').value;
-    h/=100.0;
-    let bmi = w/(h*h);
+    h /= 100.0;
+    let bmi = w / (h * h);
     bmi = parseFloat(bmi).toFixed(2);
     let img;
-    let data='';
-    if(bmi<19){
-        data='You Are Underweight';
-        img=".assets/underweight.jpg";
+    let data = '';
+    if (bmi < 18.5) {
+      data = 'You Are Underweight';
+      img = "./assets/underweight.jpg";
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+      data = 'You Are Healthy';
+      img = "./assets/healthy.jpg";
+    } else if (bmi > 24.9 && bmi <= 30) {
+      data = 'You Are Overweight';
+      img = "./assets/overweight.jpg";
+    } else if (bmi > 30) {
+      data = 'You Are Obese';
+      img = "./assets/obese.jpg";
+    } else {
+      data = 'Please Enter a Valid Input';
     }
-    else if(bmi>=19 && bmi<=25){
-        data='You Are Healthy';
-        img=".assets/healthy.jpg";
-    }
-    else if(bmi>25&&bmi<=30){
-        data='You Are Overweight';
-        img=".assets/overweight.jpg";
-    }
-    else if (bmi>30){
-        data='You Are Obese';
-        img=".assets/obese.jpg";
-    }
-    else{
-        data='Please Enter a Valid Input'
-    }
-    document.getElementById('body').setAttribute("src",img);
-    document.getElementById('res').innerHTML=` ${data}.`;
-    document.getElementById('result').innerHTML=`Your BMI is <strong>${bmi}</strong>.`;
-    document.getElementById('info').setAttribute("class","card show");
-});
+    document.getElementById('body').setAttribute('src', img);
+    document.getElementById('res').innerHTML = ` ${data}.`;
+    document.getElementById('result').innerHTML = `Your BMI is <strong>${bmi}</strong>.`;
+    document.getElementById('info').setAttribute('class', 'card show');
+  });
+  
