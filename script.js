6d5cf8e7728fea522d198948e9719ec6f882381b
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-07 08:32:25 +0530

    added scrollbar button

--- script.js
+++ script.js
@@ -100,3 +100,35 @@ fetch("https://api.github.com/repos/TusharKesarwani/Front-End-Projects/contribut
             contributorsList.appendChild(li);
         });
     });
+
+
+    //-----------for scrollbar button---------------
+
+
+    function calculateScrollValue(){
+
+        let scrollProgress=document.getElementById('progress');
+        let progressValue=document.getElementById('progress-value');
+
+        let pos=document.documentElement.scrollTop;
+        let calcHeight=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+        let scrollValue=Math.round((pos*100)/calcHeight);
+
+        // for hiding scrollbar button
+        if(pos>100){
+            scrollProgress.style.display='grid';
+        }
+        else{
+            scrollProgress.style.display='none';
+        }
+
+
+        scrollProgress.addEventListener('click',()=>{
+            document.documentElement.scrollTop=0;
+        })
+       scrollProgress.style.background=`conic-gradient(#0D4360 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+      
+    }
+    window.onscroll=calculateScrollValue;
+    window.onload=calculateScrollValue;
