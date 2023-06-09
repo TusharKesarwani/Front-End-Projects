355da9d3706a286b3dcd5bf9f49e96ef79aec72e
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-09 08:56:32 +0530

    Armstrong Checker fixed

--- Projects/ArmstrongChecker/armstrong.js
+++ Projects/ArmstrongChecker/armstrong.js
@@ -1,41 +1,18 @@
-const textinput = document.querySelector(".text");
-console.log(textinput);
-const res = document.getElementById("demo");
-const checkbtn = document.getElementById("btn");
-function textfxn() {
-    res.innerHTML = "";
-    let text = document.getElementById("text").value;
-    let newtext = text.replace(/[^0-9]/g, '');
-    let alphatext = text.replace(/[0-9]/g, '');
-    console.log(newtext, alphatext);
-
-    if (newtext && !alphatext) {
-        checkbtn.style.opacity = "1";
-        document.getElementById("btn").addEventListener("click", check);
-        checkbtn.classList.add("buttonjs");
-    }
-    else {
-        checkbtn.style.opacity = "0.25";
-        checkbtn.classList.remove("buttonjs");
-    }
-}
-
-function check() {
-    let text = document.getElementById("text").value;
-    let newtext = text.replace(/[^0-9]/g, '');
-    const numberOfDigits = newtext.length;
-    let temp = newtext;
-    let sum = 0;
+function checkArmstrong() {
+    var number = document.getElementById('numberInput').value;
+    var sum = 0;
+    var temp = number;
+  
     while (temp > 0) {
-        let remainder = temp % 10;
-        sum += remainder ** numberOfDigits;
-        temp = parseInt(temp / 10);
+      var digit = temp % 10;
+      sum += Math.pow(digit, 3);
+      temp = Math.floor(temp / 10);
     }
-    if (sum == newtext) {
-        res.innerHTML = (`${newtext} is an Armstrong number`);
+  
+    if (sum == number) {
+      document.getElementById('result').textContent = number + " is an Armstrong number.";
+    } else {
+      document.getElementById('result').textContent = number + " is not an Armstrong number.";
     }
-    else {
-        res.innerHTML = (`${newtext} is not an Armstrong number.`);
-    }
-}
-// });value;
+  }
+  
