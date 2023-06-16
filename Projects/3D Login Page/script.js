a947c65f85c250ba67ecc3d867fe5abe78022945
HimanshiDafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2023-06-16 11:41:29 +0530

    The changes made include adding visually appealing transitions to the sign-in and sign-up buttons, as well as modifying the background color of the buttons upon clicking for a more attractive and engaging user experience, while maintaining the original 3D effect and overall structure of the page.

--- Projects/3D Login Page/script.js
+++ Projects/3D Login Page/script.js
@@ -1,13 +1,26 @@
 const signUpBtn = document.querySelector(".signup-btn");
 const signInBtn = document.querySelector(".signin-btn");
 const formsWrapper = document.querySelector(".forms-wrapper");
+const signinForm = document.querySelector(".signin-form");
+const signupForm = document.querySelector(".signup-form");
 
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.add("change");
+  signupForm.classList.add("animate");
+  signinForm.classList.remove("animate");
 });
 
 signInBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.remove("change");
+  signupForm.classList.add("animate");
+  signinForm.classList.remove("animate");
 });
+
+
+  
+
+
+
+
