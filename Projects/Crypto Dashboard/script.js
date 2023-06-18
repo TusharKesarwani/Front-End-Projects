d4f0a73a23d9970c655abf4c23bb3ca168c4a6f8
Harshit Gupta <107177577+Harshitahusts@users.noreply.github.com>
2023-06-18 17:32:31 +0530

    Update script.js
    Corrected code for the addEventOnElem function:

--- Projects/Crypto Dashboard/script.js
+++ Projects/Crypto Dashboard/script.js
@@ -7,7 +7,7 @@
  */
 
 const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
+  if (elem.length >= 1) {
     for (let i = 0; i < elem.length; i++) {
       elem[i].addEventListener(type, callback);
     }
