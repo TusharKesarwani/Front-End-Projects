7fba7ff794ac6c3887994ee37953ff2f0def7f97
Harshit Gupta <107177577+Harshitahusts@users.noreply.github.com>
2023-06-18 19:50:18 +0530

    Update script.js

--- Projects/Breathe Meditation/script.js
+++ Projects/Breathe Meditation/script.js
@@ -1,5 +1,6 @@
-const container = document.getElementById('container');
-const text = document.getElementById('text');
+const container = document.querySelector('#container');
+const text = document.querySelector('#text');
+
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
@@ -9,16 +10,18 @@ breathAnimation();
 
 function breathAnimation() {
   text.innerText = 'Breathe In!';
-  container.className = 'container grow';
+  container.classList.add('grow');
 
   setTimeout(() => {
     text.innerText = 'Hold';
 
     setTimeout(() => {
       text.innerText = 'Breathe Out!';
-      container.className = 'container shrink';
+      container.classList.remove('grow');
+      container.classList.add('shrink');
     }, holdTime);
   }, breatheTime);
 }
+
 
 setInterval(breathAnimation, totalTime);
