5724021e186a17f43761240ba88da63a444e914b
HimanshiDafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2023-06-20 12:42:22 +0530

    changed the UI of Bookmark-App

--- Projects/Bookmarker_App/main.js
+++ Projects/Bookmarker_App/main.js
@@ -1,7 +1,8 @@
-//form submission
 document.getElementById('myform').addEventListener('submit', savebookmark);
+
 function savebookmark(e) {
-    //get form values
+    e.preventDefault();
+
     var sitename = document.getElementById('sitename').value;
     var siteurl = document.getElementById('siteurl').value;
 
@@ -9,18 +10,16 @@ function savebookmark(e) {
         return false;
     }
 
-    // creating object to put in local storage
     var bookmark = {
         name: sitename,
         url: siteurl
-    }
+    };
 
     if (localStorage.getItem('bookmarks') === null) {
         var bookmarks = [];
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }
-    else {
+    } else {
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
@@ -28,57 +27,58 @@ function savebookmark(e) {
 
     document.getElementById('myform').reset();
     fetchbookmarks();
-    //prevents form submission
-    e.preventDefault();
 }
 
 function deletebookmark(i) {
-    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     bookmarks.splice(i, 1);
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     fetchbookmarks();
 }
 
-//fetch bookmarks
 function fetchbookmarks() {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    //console.log(bookmarks);
     var bookmarkresults = document.getElementById('bookmarksresults');
 
     bookmarkresults.innerHTML = '';
-    for (var i = 0; i < bookmarks.length; i++) {
-        var name = bookmarks[i].name;
-        var url = bookmarks[i].url;
 
-        bookmarkresults.innerHTML += `<div class="well"> 
-        <div class="row">
-            <div class="col-9">
-                <h5>${bookmarks[i].name}</h5>    
-            </div>
-            <div class="col-3">
-                <a class="btn btn-sm btn-success" href="https://${bookmarks[i].url}" target="_blank">Visit</a>
-                <a onclick="deletebookmark(${i});" class="btn btn-danger text-light btn-sm" >Delete</a> 
-            </div>
-        </div>   
-        <small>Created at : ${Date().slice(4, 15)}</small>                    
-        </div>`;
+    if (bookmarks && bookmarks.length > 0) {
+        for (var i = 0; i < bookmarks.length; i++) {
+            var name = bookmarks[i].name;
+            var url = bookmarks[i].url;
+
+            bookmarkresults.innerHTML += `
+                <div class="well">
+                    <h5>${name}</h5>
+                    <div class="button-group">
+                        <a class="btn btn-success" href="https://${url}" target="_blank">Visit</a>
+                        <a class="btn btn-danger" onclick="deletebookmark(${i});">Delete</a>
+                    </div>
+                    <small>Created at: ${new Date().toLocaleDateString()}</small>
+                </div>
+            `;
+        }
+    } else {
+        bookmarkresults.innerHTML = '<p>No bookmarks available</p>';
     }
 }
 
-// Validate Form
 function validateform(sitename, siteurl) {
     if (!sitename || !siteurl) {
-        alert('Please fill the form !!!');
+        alert('Please fill in all fields.');
         return false;
     }
 
-    // using regular expression here  for valid url 
-    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var expression = /^((http|https):\/\/)?([A-Za-z0-9]+\.[A-Za-z]{2,})+(\/.*)?$/;
     var regex = new RegExp(expression);
 
     if (!siteurl.match(regex)) {
-        alert('Please use a valid URL')
+        alert('Please use a valid URL');
         return false;
     }
+
     return true;
 }
+
+// Fetch bookmarks on page load
+document.addEventListener('DOMContentLoaded', fetchbookmarks);
