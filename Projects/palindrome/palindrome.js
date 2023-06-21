e9377559cb2311465a79de62a51ee0f99a411d98
Akshit <103023426+akshit20421@users.noreply.github.com>
2023-06-22 02:00:27 +0530

    Palindrome UI --changes

--- Projects/palindrome/palindrome.js
+++ Projects/palindrome/palindrome.js
@@ -1,25 +1,28 @@
 function palindrome_check(){
-    var str1 = document.getElementById("input").value;
-    var str=str1.replace(/\s/g, "");
-    if(isPalindrome(str)===true)
-    document.getElementById("prog").innerHTML = `<b>"${str}"</b> is a palindrome!.`;
-    else
-    document.getElementById("prog").innerHTML = `<b>"${str}"</b> is not a palindrome!.`;
+  var str1 = document.getElementById("input").value;
+  var str=str1.replace(/\s/g, "");
+  if(isPalindrome(str)===true){
+  document.getElementById("prog").innerHTML = `<b>"${str}"</b> is a palindrome!.`;
+  document.getElementById("input").value="";
+  
+}
+  else
+  document.getElementById("prog").innerHTML = `<b>"${str}"</b> is not a palindrome!.`;
 }
 function isPalindrome(str1) {
-    str=str1.toLowerCase();
-    for (let i = 0; i < Math.floor(str.length / 2); i++)
+  str=str1.toLowerCase();
+  for (let i = 0; i < Math.floor(str.length / 2); i++)
+  {
+    var char1=str[i],char2=str[str.length - 1 - i];
+    if (char1 !== char2)
     {
-      var char1=str[i],char2=str[str.length - 1 - i];
-      if (char1 !== char2)
-      {
-        return false;
-      }
-
+      return false;
     }
-    return true;
+
+  }
+  return true;
 }
 function reset(){
-    document.getElementById("input").value="";
-    document.getElementById("prog").innerHTML="";
+  document.getElementById("input").value="";
+  document.getElementById("prog").innerHTML="";
 }
