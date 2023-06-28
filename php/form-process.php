cfb5efdd2b77bb96f6f6643bdefb5669ce8da6f5
VEER YADAV <112725639+codingwithveer18@users.noreply.github.com>
2023-06-29 00:33:09 +0530

    #1007

---
+++ php/form-process.php
@@ -0,0 +1,10 @@
+<?php
+$mongo= new MongoClient();
+$db=$mongo->formcontent;
+$collection=$db->Documents;
+if($_POST)
+{
+    $insert=>$_POST['name'],
+    $insert=>$_POST['name'],
+    $insert=>$_POST['name'],
+}
