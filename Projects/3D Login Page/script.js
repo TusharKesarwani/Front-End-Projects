e1523c9a89a75294875360e08552651cdebafea2
HimanshiDafouty <116953498+HimanshiDafouty@users.noreply.github.com>
2023-07-07 19:09:55 +0530

    Updated the changes.

--- Projects/3D Login Page/script.js
+++ Projects/3D Login Page/script.js
@@ -1,13 +1,19 @@
 const signUpBtn = document.querySelector(".signup-btn");
 const signInBtn = document.querySelector(".signin-btn");
 const formsWrapper = document.querySelector(".forms-wrapper");
+const signInForm = document.querySelector(".signin-form");
+const signUpForm = document.querySelector(".signup-form");
 
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.add("change");
+  signInForm.classList.add("hidden");
+  signUpForm.classList.remove("hidden");
 });
 
 signInBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formsWrapper.classList.remove("change");
+  signUpForm.classList.add("hidden");
+  signInForm.classList.remove("hidden");
 });
