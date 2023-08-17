c184fea7ce6aa49cdfe4b3327069f8bd0ceed24e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-17 05:27:43 +0100

    [Revamp Meme Generator UI] - Added nice fonts - Made application responsive - Updated README with project new UI

--- Projects/Meme Generator/main.js
+++ Projects/Meme Generator/main.js
@@ -3,7 +3,6 @@ const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
 const loader = document.querySelector(".loader-container");
-const downloadBtn = document.querySelector(".download-meme-btn");
 
 const updateDetails = (url, title, author) => {
   memeImage.setAttribute("src", url);
@@ -25,32 +24,5 @@ const generateMeme = () => {
     });
 };
 
-generateMeme();
-
-const downloadMeme = () => {
-  const imageUrl = memeImage.getAttribute("src");
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  const img = new Image();
-
-  // Set up a cross-origin attribute to avoid CORS issues
-  img.setAttribute("crossorigin", "anonymous");
-
-  img.onload = function () {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0, img.width, img.height);
-    const dataUrl = canvas.toDataURL("image/jpeg");
-    const link = document.createElement("a");
-    link.href = dataUrl;
-    link.download = `${localStorage.getItem("memeTitle")}.jpg` || "meme.jpg";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  img.src = imageUrl;
-};
-
-downloadBtn.addEventListener("click", downloadMeme);
 generateMemeBtn.addEventListener("click", generateMeme);
+generateMeme();
