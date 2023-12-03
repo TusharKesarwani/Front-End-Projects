a5e3eeb6dd102cb0790164e3170b37d30d0b386f
Linda Septira Andryani <linda.septira.andryani@mni.thm.de>
2023-12-03 21:27:32 +0100

    Point of Sale Design

---
+++ Projects/Point-Of-Sale_For_Resto/assets/custom js/script.js
@@ -0,0 +1,15 @@
+$(".sidebar ul li").on('click', function () {
+    $(".sidebar ul li.active").removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.open-btn').on('click', function () {
+    $('.sidebar').addClass('active');
+
+});
+
+
+$('.close-btn').on('click', function () {
+    $('.sidebar').removeClass('active');
+
+})
